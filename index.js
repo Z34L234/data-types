e54b7e19b0360f75d2ff05591e3951880419f71b
Z34L234 <nestyu66@gmail.com>
2022-07-21 13:55:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log ("hello")
+let Apple= "Ernest" 
+console.log(Apple)
+let name= "Ernest" +" "+ "Onyekwena"
+console.log (name)
+let x= 2
+let y= 4
+let add= x+y
+let subtract= y-x
+let divide= y/x
+let multiply= y*x
+console.log (multiply)
+console.log (add)
+console.log (subtract)
+console.log (divide)
+let equals= x==y
+console.log (equals)
+let lessthan= x<y
+console.log (lessthan)
